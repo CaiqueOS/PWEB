f1d265604ba93a48fa903290de9ee7cf0d9955ac
CaiqueOS <78798335+CaiqueOS@users.noreply.github.com>
2022-05-09 12:24:58 -0300

    Add files via upload

---
+++ Atividade 10/script.js
@@ -0,0 +1,18 @@
+var aluno = new Object();
+aluno.RA = 123456789;
+aluno.nome = "Caique Oliveira";
+
+var aluno1 = {};
+aluno1.RA = 123321123;
+aluno1.nome = "Edson Isaac";
+
+var aluno2 = {
+    RA : 123454321,
+    nome : "Barbara Aparecida"
+};
+
+console.log(aluno);
+console.log(aluno1);
+console.log(aluno2);
+
+alert("Checar Console para verificar objetos!");
