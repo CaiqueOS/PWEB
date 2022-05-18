2d4392de89cda8d18c9d49b787524e638eeaf1a1
CaiqueOS <78798335+CaiqueOS@users.noreply.github.com>
2022-05-18 12:26:48 -0300

    Add files via upload

---
+++ Atividade 12/script.js
@@ -0,0 +1,17 @@
+const janela = document.getElementById("janela");
+const texto = document.getElementById("lblJanela");
+
+function abre(){
+    janela.src = "./img/janelaaberta.png";
+    texto.innerHTML = "Janela está aberta!";
+}
+
+function quebra(){
+    janela.src = "./img/janelaquebra.png";
+    texto.innerHTML = "Você quebrou a janela!";
+}
+
+function fecha(){
+    janela.src = "./img/janelafechada.png";
+    texto.innerHTML = "Abra a janela!";
+}
